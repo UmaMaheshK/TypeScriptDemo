1a029b02bf9cbb10bd0ca28e834b159320efc1c7
bhavana <bhavanareddy1712@gmail.com>
2018-02-10 10:06:48 +0530

    Save All Classes

---
+++ Classes/Part1.js
@@ -0,0 +1,68 @@
+var First = /** @class */ (function () {
+    function First() {
+        console.log('Welcome to classes');
+    }
+    return First;
+}());
+var f = new First(); //Welcome to classes
+var Second = /** @class */ (function () {
+    function Second(id, name) {
+        this.a = id;
+        this.name = name;
+    }
+    //method with default return type void
+    Second.prototype.displayData = function () {
+        console.log("a:=" + this.a + "\nname:=" + this.name);
+    };
+    return Second;
+}());
+var s = new Second(100, 'Uma Mahesh');
+s.displayData();
+var Three = /** @class */ (function () {
+    //Note id and name is not accessiable in local and outside
+    function Three(id, name) {
+        console.log("id:=" + id + "\nname:=" + name);
+    }
+    Three.prototype.getData = function () {
+        return 'Welcome';
+    };
+    return Three;
+}());
+var t = new Three(120, 'Uma');
+console.log(t.getData()); //Welcome
+var Four = /** @class */ (function () {
+    function Four(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    Four.prototype.getId = function () {
+        return this.id;
+    };
+    Four.prototype.getName = function () {
+        return this.name;
+    };
+    return Four;
+}());
+var fo = new Four(130, 'Hello');
+console.log(fo.getId()); //130
+console.log(fo.getName()); //Hello
+var Fifth = /** @class */ (function () {
+    function Fifth(id, name) {
+        if (name === void 0) { name = 'Suma Latha'; }
+        this.id = id;
+        this.name = name;
+    }
+    Fifth.prototype.getId = function () {
+        return this.id;
+    };
+    Fifth.prototype.getName = function () {
+        return this.name;
+    };
+    return Fifth;
+}());
+var F = new Fifth(111);
+console.log(F.getId()); //111
+console.log(F.getName()); //Suma Latha
+F = new Fifth(100, 'Mahesh');
+console.log(F.getId()); //111
+console.log(F.getName()); //Suma Latha
--- Classes/Part1.ts
+++ Classes/Part1.ts
@@ -3,3 +3,65 @@ class First {
         console.log('Welcome to classes');
     }
 }
+
+let f = new First();//Welcome to classes
+
+class Second {
+    private a: number;
+    private name: string;
+    constructor(id: number, name: string) {
+        this.a = id;
+        this.name = name;
+    }
+    //method with default return type void
+    displayData() {
+        console.log(`a:=${this.a}\nname:=${this.name}`);
+    }
+}
+
+const s = new Second(100, 'Uma Mahesh');
+s.displayData();
+
+class Three {
+    //Note id and name is not accessiable in local and outside
+    constructor(id: number, name: string) {
+        console.log(`id:=${id}\nname:=${name}`);
+    }
+
+    getData(): string {
+        return 'Welcome';
+    }
+}
+const t = new Three(120, 'Uma');
+console.log(t.getData());//Welcome
+
+class Four {
+    constructor(private id: number, private name: string) {
+    }
+    getId(): number {
+        return this.id;
+    }
+    getName(): string {
+        return this.name;
+    }
+}
+const fo = new Four(130, 'Hello');
+console.log(fo.getId());//130
+console.log(fo.getName());//Hello
+
+class Fifth {
+    constructor(private id: number, private name: string = 'Suma Latha') { }
+    getId(): number {
+        return this.id;
+    }
+    getName(): string {
+        return this.name;
+    }
+}
+let F = new Fifth(111);
+console.log(F.getId());//111
+console.log(F.getName());//Suma Latha
+
+F = new Fifth(100, 'Mahesh');
+console.log(F.getId());//111
+console.log(F.getName());//Suma Latha
