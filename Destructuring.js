ebbef84beff671c7402ed3a439bd8903e0cc303b
bhavana <bhavanareddy1712@gmail.com>
2018-02-12 08:39:27 +0530

    Spread

--- Destructuring.js
+++ Destructuring.js
@@ -1,3 +1,21 @@
+//Array Destructuring
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) if (e.indexOf(p[i]) < 0)
+            t[p[i]] = s[p[i]];
+    return t;
+};
 var input = [10, 12];
 var a = input[0], b = input[1];
 console.log(a); //10
@@ -28,4 +46,78 @@ var _b = [1, 2, 3, 4, 5], l = _b[0], n = _b[1], rest = _b.slice(2);
 console.log(l); //1
 console.log(2); //2
 console.log(rest); //[3,4,5]
-var _a;
+//Exp4
+var _c = [1, 2, 3, 5], i = _c[1], k = _c[3];
+console.log(i); //2
+console.log(k); //5
+//Object Destructuring
+var o = {
+    a1: "foo",
+    b1: 12,
+    c: "bar"
+};
+var a1 = o.a1, b1 = o.b1;
+console.log(a1);
+console.log(b1);
+(_d = { a1: "baz", b1: 101 }, a1 = _d.a1, b1 = _d.b1);
+var emp = { empId: 100, empName: 'Hello' };
+//property rename
+var Id = emp.empId, FullName = emp.empName;
+console.log('Id: = ', Id);
+console.log('FullName: = ', FullName);
+var rect = { x1: 0, y1: 10, width: 15, height: 20 };
+// Destructuring assignment
+var x1 = rect.x1, y1 = rect.y1, width = rect.width, height = rect.height;
+console.log(x, y, width, height); // 0,10,15,20
+rect.x1 = 10;
+(x1 = rect.x1, y1 = rect.y1, width = rect.width, height = rect.height); // assign to existing variables using outer parentheses
+console.log(x1, y1, width, height); // 10,10,15,20
+var foo = { bar: { bas: 123 } };
+var bas = foo.bar.bas; // Effectively `var bas = foo.bar.bas;`
+console.log(bas);
+var _e = { w: 1, x2: 2, y: 3, z: 4 }, w = _e.w, x2 = _e.x2, remaining = __rest(_e, ["w", "x2"]);
+console.log(w, x2, remaining); // 1, 2, {y:3,z:4}
+//Default values
+function added(myobj) {
+    var a = myobj.a, _a = myobj.b, b = _a === void 0 ? 200 : _a;
+    console.log(a, b);
+}
+added({ a: 1, b: 2 }); //1,2
+added({ a: 1 }); //1 200
+function f(_a) {
+    var a = _a.a, b = _a.b;
+    console.log(a, b); //
+}
+//let temp:C={a:133};
+f({ a: '150', b: 144 });
+f({ a: '150' });
+function default1(_a) {
+    var _b = _a === void 0 ? { a: '', b: 0 } : _a, a = _b.a, b = _b.b;
+    console.log(a, b); //'' 0
+}
+default1();
+function default2(_a) {
+    var _b = _a === void 0 ? { a: '' } : _a, a = _b.a, _c = _b.b, b = _c === void 0 ? 0 : _c;
+    console.log(a, b); //'' 0
+}
+default2();
+//Spread Array
+var arr1 = [1, 2, 3, 4];
+var arr2 = [5, 6, 7, 8];
+var arrBoth = arr1.concat(arr2);
+console.log(arrBoth); //[1,2,3,4,5,6,7,8]
+var arr3 = [1, 2, 3];
+var arr4 = [1, 4, 5];
+var arr34 = arr3.concat(arr4);
+console.log(arr34); //[ 1, 2, 3, 1, 4, 5 ]
+var arr5 = [1, 2, 3];
+var arr6 = [4, 5, 6];
+var arr56 = arr5.concat([20], arr6, [90]);
+console.log(arr56); //[1,2,3,20,4,5,6,90]
+//Spread Object
+var defaults = { food: "spicy", price: "$$", ambiance: "noisy" };
+var search = __assign({}, defaults, { food: "rich" });
+console.log(search); //{ food: 'rich', price: '$$', ambiance: 'noisy' }
+var search123 = __assign({ food: 'rich' }, defaults);
+console.log(search);
+var _a, _d;
--- Destructuring.ts
+++ Destructuring.ts
@@ -54,4 +54,78 @@ let { a1, b1 } = o;
 console.log(a1);
 console.log(b1);
 
-// ({ al, b } = { al: "baz", b: 101 });
+({ a1, b1 } = { a1: "baz", b1: 101 });
+
+let emp = { empId: 100, empName: 'Hello' };
+//property rename
+let { empId: Id, empName: FullName } = emp;
+console.log('Id: = ', Id);
+console.log('FullName: = ', FullName);
+
+var rect = { x1: 0, y1: 10, width: 15, height: 20 };
+
+// Destructuring assignment
+let { x1, y1, width, height } = rect;
+console.log(x, y, width, height); // 0,10,15,20
+
+rect.x1 = 10;
+({ x1, y1, width, height } = rect); // assign to existing variables using outer parentheses
+console.log(x1, y1, width, height); // 10,10,15,20
+
+let foo = { bar: { bas: 123 } };
+let { bar: { bas } } = foo; // Effectively `var bas = foo.bar.bas;`
+console.log(bas);
+
+let { w, x2, ...remaining } = { w: 1, x2: 2, y: 3, z: 4 };
+console.log(w, x2, remaining); // 1, 2, {y:3,z:4}
+
+//Default values
+function added(myobj: { a: number, b?: number }): void {
+    let { a, b = 200 } = myobj;
+    console.log(a, b);
+}
+added({ a: 1, b: 2 });//1,2
+added({ a: 1 });//1 200
+
+//Function declarations
+type C = { a: string, b?: number }
+function f({ a, b }: C): void {
+    console.log(a, b);//
+}
+//let temp:C={a:133};
+f({ a: '150', b: 144 });
+f({ a: '150' });
+
+function default1({ a, b }: C = { a: '', b: 0 }): void {
+    console.log(a, b);//'' 0
+}
+default1();
+function default2({ a, b = 0 }: C = { a: '' }): void {
+    console.log(a, b);//'' 0
+}
+default2();
+
+//Spread Array
+
+let arr1 = [1, 2, 3, 4];
+let arr2 = [5, 6, 7, 8];
+let arrBoth = [...arr1, ...arr2];
+console.log(arrBoth);//[1,2,3,4,5,6,7,8]
+
+let arr3 = [1, 2, 3];
+let arr4 = [1, 4, 5];
+let arr34 = [...arr3, ...arr4];
+console.log(arr34);//[ 1, 2, 3, 1, 4, 5 ]
+
+let arr5 = [1, 2, 3];
+let arr6 = [4, 5, 6];
+let arr56 = [...arr5, 20, ...arr6, 90];
+console.log(arr56);//[1,2,3,20,4,5,6,90]
+
+//Spread Object
+let defaults = { food: "spicy", price: "$$", ambiance: "noisy" };
+let search = { ...defaults, food: "rich" };
+console.log(search);//{ food: 'rich', price: '$$', ambiance: 'noisy' }
+
+let search123 = { food: 'rich', ...defaults };
+console.log(search);//{ food: 'rich', price: '$$', ambiance: 'noisy' }
